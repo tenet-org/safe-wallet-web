00772994b973cf269b3253ea1d3fb994486693b0
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2023-08-16 14:44:36 +0300

    feat: add suggestion button to the sidebar

--- src/components/sidebar/SidebarFooter/index.tsx
+++ src/components/sidebar/SidebarFooter/index.tsx
@@ -11,14 +11,18 @@ import { BEAMER_SELECTOR, loadBeamer } from '@/services/beamer'
 import { useAppDispatch, useAppSelector } from '@/store'
 import { selectCookies, CookieType } from '@/store/cookiesSlice'
 import { openCookieBanner } from '@/store/popupSlice'
-import BeamerIcon from '@/public/images/sidebar/whats-new.svg'
-import HelpCenterIcon from '@/public/images/sidebar/help-center.svg'
+// import BeamerIcon from '@/public/images/sidebar/whats-new.svg'
+import SuggestionIcon from '@/public/images/sidebar/lightbulb_icon.svg'
+// import HelpCenterIcon from '@/public/images/sidebar/help-center.svg'
 import { ListItem } from '@mui/material'
 import DebugToggle from '../DebugToggle'
-import { HELP_CENTER_URL, IS_PRODUCTION } from '@/config/constants'
+import { /* HELP_CENTER_URL, */ IS_PRODUCTION } from '@/config/constants'
 import Track from '@/components/common/Track'
 import { OVERVIEW_EVENTS } from '@/services/analytics/events/overview'
 import { useCurrentChain } from '@/hooks/useChains'
+
+const SUGGESTION_PATH =
+  'https://docs.google.com/forms/d/e/1FAIpQLSfojsADYCiWq9AqbLqsUTzCDSpA8FMgdAQp0Pyl0BOeurlq9A/viewform?usp=sf_link'
 
 const SidebarFooter = (): ReactElement => {
   const dispatch = useAppDispatch()
@@ -48,7 +52,7 @@ const SidebarFooter = (): ReactElement => {
         </ListItem>
       )}
 
-      <Track {...OVERVIEW_EVENTS.WHATS_NEW}>
+      {/* <Track {...OVERVIEW_EVENTS.WHATS_NEW}>
         <ListItem disablePadding>
           <SidebarListItemButton id={BEAMER_SELECTOR} onClick={handleBeamer}>
             <SidebarListItemIcon color="primary">
@@ -67,6 +71,22 @@ const SidebarFooter = (): ReactElement => {
                 <HelpCenterIcon />
               </SidebarListItemIcon>
               <SidebarListItemText bold>Need help?</SidebarListItemText>
+            </SidebarListItemButton>
+          </a>
+        </ListItem>
+      </Track>*/}
+      <Track {...OVERVIEW_EVENTS.SUGGESTIONS}>
+        <ListItem disablePadding>
+          <a target="_blank" rel="noopener noreferrer" href={SUGGESTION_PATH} style={{ width: '100%' }}>
+            <SidebarListItemButton
+              id={BEAMER_SELECTOR}
+              style={{ backgroundColor: '#12FF80', color: 'black' }}
+              onClick={handleBeamer}
+            >
+              <SidebarListItemIcon color="primary">
+                <SuggestionIcon />
+              </SidebarListItemIcon>
+              <SidebarListItemText bold>New Features Suggestion?</SidebarListItemText>
             </SidebarListItemButton>
           </a>
         </ListItem>
